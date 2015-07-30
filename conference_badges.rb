1438a6b59b70d6484cf52c9f7e8067884da94f9f
Tyler Machen <tylermachen@gmail.com>
2015-07-30 17:43:11 -0400

    create badge methods

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.map { |attendee| badge_maker(attendee) }
+end
+
+def assign_rooms(speakers) 
+  speakers.each_with_index.map { |s, i| "Hello, #{s}! You'll be assigned to room #{i + 1}!" }
+end
+
+def printer(speakers)
+  batch_badge_creator(speakers).each { |s| puts s }
+  assign_rooms(speakers).each { |s| puts s }
+end
